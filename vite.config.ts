76e2ee39db20d88eca74f512bdab8549eb0122cb
totewiwittaporn <totee.wiwittaporn@gmail.com>
2025-10-07 18:59:08 +0700

    up

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { fileURLToPath } from "url";
-import { componentTagger } from "lovable-tagger";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-    base: "/chalin-shop-system/",
-
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(
-    Boolean
-  ),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+export default defineConfig(() => ({
+  base: "/chalin-shop-system/",
+  server: { host: "::", port: 8080 },
+  plugins: [react()], // 👈 ชั่วคราวตัดทุก plugin อื่นออก
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 }));
