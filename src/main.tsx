f4751f8f4c687d213dfe30744ca5b7afd8ce4c4a
totewiwittaporn <totee.wiwittaporn@gmail.com>
2025-10-07 18:36:56 +0700

    up

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
+// src/main.tsx
+import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+// เปลี่ยนมาใช้ HashRouter
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css"; // ถ้ามี global styles/Tailwind
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
